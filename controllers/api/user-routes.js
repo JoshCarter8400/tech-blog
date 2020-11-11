0d9ae70b664ec438047fc4e72ab85c0a566144ec
Josh Carter <joshcarter8400@gmail.com>
2020-11-11 08:23:58 -0500

    fixed login error

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -70,27 +70,27 @@ router.post("/", (req, res) => {
 });
 
 router.post("/login", (req, res) => {
-  console.log("New President");
+  // findOne method by email to look for an existing user in the database with the email address entered
   // expects {email: 'lernantino@gmail.com', password: 'password1234'}
   User.findOne({
     where: {
       email: req.body.email,
     },
   }).then((dbUserData) => {
+    // if the email is not found, return an error
     if (!dbUserData) {
       res.status(400).json({ message: "No user with that email address!" });
       return;
     }
-
-    // res.json({ user: dbUserData });
-
-    // Verify user
+    // Otherwise, verify the user.
+    // call the instance method as defined in the User model
     const validPassword = dbUserData.checkPassword(req.body.password);
+    // if the password is invalid (method returns false), return an error
     if (!validPassword) {
       res.status(400).json({ message: "Incorrect password!" });
       return;
     }
-
+    // otherwise, save the session, and return the user object and a success message
     req.session.save(() => {
       // declare session variables
       req.session.user_id = dbUserData.id;
--- public/javascript/login.js
+++ public/javascript/login.js
@@ -26,26 +26,31 @@ async function signupFormHandler(event) {
   }
 }
 
+// Login form handler
 async function loginFormHandler(event) {
   event.preventDefault();
 
+  // get the information from the login form
   const email = document.querySelector("#email-login").value.trim();
   const password = document.querySelector("#password-login").value.trim();
 
+  // if both fields have content
   if (email && password) {
+    // POST to the login route with the user information
     const response = await fetch("/api/users/login", {
-      method: "POST",
+      method: "post",
       body: JSON.stringify({
         email,
         password,
       }),
       headers: { "Content-Type": "application/json" },
     });
-
+    // when the fetch promise is fufilled, check the response status; if the response is good, load the dashboard; if there is an error, alert with the status
     if (response.ok) {
-      document.location.replace("/dashboard/");
+      document.location.replace("/dashboard");
     } else {
-      alert(response.statusText);
+      let result = await response.json();
+      alert(result.message);
     }
   }
 }
